f6a2942f8c22fea7d7976a706f12262cf9930bf0
Brian N <brian.nguyen93@gmail.com>
2025-02-13 08:39:01 +1100

    Fix tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          bloombergBg: "#121212", // Dark background
-          bloombergTable: "#1E1E1E", // Table background
-          bloombergText: "#E0E0E0", // Light gray text
-          bloombergAccent: "#F2994A", // Orange highlight
-        },
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Adjust paths as needed
+  ],  
+  theme: {
+    extend: {
+      colors: {
+        bloombergBg: "#121212", // Dark background
+        bloombergTable: "#1E1E1E", // Table background
+        bloombergText: "#E0E0E0", // Light gray text
+        bloombergAccent: "#F2994A", // Orange highlight
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
